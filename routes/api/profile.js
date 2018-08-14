bac90b965c27771429778dd11f0884a39352ffaa
Nick Akrap <nakrap@gmail.com>
2018-08-14 16:43:19 -0500

    created profile model and route to profile.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,37 @@
 const express = require("express");
-
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load Profile Model:
+const Profile = require("../../models/Profile");
+// Load User Model:
+const User = require("../../models/User");
 
 // Route =          GET api/profile/test
 // Description =    Tests 'profile' route
 // Access =         PUBLIC
 router.get("/test", (req, res) => res.json({ msg: "Profile works!" }));
+
+// Route =          GET api/profile
+// Description =    Get current users profile page
+// Access =         PRIVATE
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user.";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
